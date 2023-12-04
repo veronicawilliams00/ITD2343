6251ce517d63f84b1b89066c76775898bed7fd6b
veronicawilliams00 <73403290+veronicawilliams00@users.noreply.github.com>
2023-12-04 12:28:11 -0600

    Add files via upload

---
+++ frmCheckDigit.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace frmCheckDigitProject
+{
+    partial class frmCheckDigit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.lblConfirm = new System.Windows.Forms.Label();
+            this.lblPayment = new System.Windows.Forms.Label();
+            this.txtAcc = new System.Windows.Forms.TextBox();
+            this.txtCon = new System.Windows.Forms.TextBox();
+            this.txtPay = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Plum;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(316, 363);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(121, 36);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Plum;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(462, 363);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 36);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnProcess.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(29, 363);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(121, 36);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "&Process";
+            this.btnProcess.UseVisualStyleBackColor = false;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(169, 363);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(121, 36);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.BackColor = System.Drawing.Color.LightPink;
+            this.lblAccount.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccount.Location = new System.Drawing.Point(100, 24);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(159, 23);
+            this.lblAccount.TabIndex = 4;
+            this.lblAccount.Text = "Account Number:";
+            // 
+            // lblConfirm
+            // 
+            this.lblConfirm.AutoSize = true;
+            this.lblConfirm.BackColor = System.Drawing.Color.LightPink;
+            this.lblConfirm.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirm.Location = new System.Drawing.Point(25, 64);
+            this.lblConfirm.Name = "lblConfirm";
+            this.lblConfirm.Size = new System.Drawing.Size(234, 23);
+            this.lblConfirm.TabIndex = 5;
+            this.lblConfirm.Text = "Confirm Account Number:";
+            // 
+            // lblPayment
+            // 
+            this.lblPayment.AutoSize = true;
+            this.lblPayment.BackColor = System.Drawing.Color.LightPink;
+            this.lblPayment.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPayment.Location = new System.Drawing.Point(100, 103);
+            this.lblPayment.Name = "lblPayment";
+            this.lblPayment.Size = new System.Drawing.Size(159, 23);
+            this.lblPayment.TabIndex = 6;
+            this.lblPayment.Text = "Payment Amount:";
+            // 
+            // txtAcc
+            // 
+            this.txtAcc.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAcc.Location = new System.Drawing.Point(284, 23);
+            this.txtAcc.Name = "txtAcc";
+            this.txtAcc.Size = new System.Drawing.Size(293, 26);
+            this.txtAcc.TabIndex = 7;
+            // 
+            // txtCon
+            // 
+            this.txtCon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCon.Location = new System.Drawing.Point(284, 64);
+            this.txtCon.Name = "txtCon";
+            this.txtCon.Size = new System.Drawing.Size(293, 26);
+            this.txtCon.TabIndex = 8;
+            // 
+            // txtPay
+            // 
+            this.txtPay.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPay.Location = new System.Drawing.Point(284, 103);
+            this.txtPay.Name = "txtPay";
+            this.txtPay.Size = new System.Drawing.Size(293, 26);
+            this.txtPay.TabIndex = 9;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.Lavender;
+            this.lblStatus.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(25, 146);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(558, 205);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Welcome!";
+            // 
+            // frmCheckDigit
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(611, 426);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtPay);
+            this.Controls.Add(this.txtCon);
+            this.Controls.Add(this.txtAcc);
+            this.Controls.Add(this.lblPayment);
+            this.Controls.Add(this.lblConfirm);
+            this.Controls.Add(this.lblAccount);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmCheckDigit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check Digit Verificatoin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblConfirm;
+        private System.Windows.Forms.Label lblPayment;
+        private System.Windows.Forms.TextBox txtAcc;
+        private System.Windows.Forms.TextBox txtCon;
+        private System.Windows.Forms.TextBox txtPay;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ frmCheckDigit.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmCheckDigitProject
+{
+    public partial class frmCheckDigit : Form
+    {
+        public frmCheckDigit()
+        {
+            InitializeComponent();
+        }
+        /**********************************************
+        * Name: Veronica Williams
+        * Class: ITD-2343 Obj. Oriented Programming C#
+        * Instructor: Greg Morris
+        * Due Date: 11/07/2023
+        * *********************************************/
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAcc.Text = " ";
+            txtCon.Text = " ";
+            txtPay.Text = " ";
+            lblStatus.Text = " ";
+
+            txtAcc.Focus();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                decimal txtPay = 0m;
+
+                if (txtAcc.Text != txtCon.Text)
+                {
+                    lblStatus.Text = "Please reconfirm you account numbers.";
+                }
+                else if (txtAcc.MaxLength > 10)
+                {
+                    lblStatus.Text = "Number is too long.";
+                }
+                else if (txtPay == 0)
+                {
+                    lblStatus.Text = "Please enter a value greater than 0.";
+                }
+                else
+                {
+                    DateTime currentDate = DateTime.Today;
+
+                    lblStatus.Text = "Date: " + currentDate.ToLongDateString() + "\n" + "\n" + "Your Account: " + txtAcc + "\n" + "Your payment of: " + txtPay + "\n" + "Will be processed. Thank you!";
+
+                }
+            }
+
+            catch(Exception)
+            {
+                lblStatus.Text = "Please enter the required information.";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = " ";
+        }
+    }
+}
